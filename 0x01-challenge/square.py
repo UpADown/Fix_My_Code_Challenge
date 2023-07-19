96b5aa4f0596c75d5059e9b52db836737d6ee589
abenezer-niguse <abenezerniguse777@gmail.com>
2023-07-18 17:32:00 -0700

    debugging

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,39 +1,51 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+class square(object):
+    """A class that represents a square with width and height attributes.
 
-"""
-Square class module
-Usage: ./square.py
-"""
-
-
-class square():
+    Attributes:
+        width (int): The width of the square.
+        height (int): The height of the square.
     """
-    The Square class
-    """
-    width = 0
-    height = 0
 
     def __init__(self, *args, **kwargs):
-        """The constructor"""
+        """Initialize the square with default or given width and height values.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """Calculate and return the area of the square.
 
-    def perimeter_of_the_square(self):
-        """Perimeter of the square"""
+        Returns:
+            int: The area of the square.
+        """
+        return self.width * self.height
+
+    def perimeter_of_my_square(self):
+        """Calculate and return the perimeter of the square.
+
+        Returns:
+            int: The perimeter of the square.
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """String reresentation of the square"""
+        """Return a string representation of the square.
+
+        Returns:
+            str: A string in the format "width/height".
+        """
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
-    """This should be fun"""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_the_square())
+    print(s.perimeter_of_my_square())
+
